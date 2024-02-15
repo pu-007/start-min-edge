272a8076290fac760390ecb2b9c1329c378200b5
pu-007 <pu.007@qq.com>
2024-02-15 22:22:43 +0800

    start glazewm at startup (maybe safe for no glazewm)

--- README.md
+++ README.md
@@ -9,10 +9,4 @@ Set WshShell = CreateObject("WScript.Shell")
     WshShell.Run"python /path/to/start-min-edge", 0, False
 ```
 
-You can also use `glazewm` to hide the window.
-
-```yaml
-- command: "move to workspace 9"
-  match_title: "/C0BF384C333E479C9C01884375881FBC.+/"
-  match_class_name: "Chrome_WidgetWin_1"
-```
+support for `glazewm`, it will store the window to workspace 9.
--- __main__.py
+++ __main__.py
@@ -1,11 +1,23 @@
 import subprocess
 import pygetwindow as gw
 import os
+from time import sleep
 
 
 def get_cwd():
     return os.path.dirname(os.path.realpath(__file__))
 
+
+subprocess.Popen(
+    [r"D:\Source\GlazeWM\GlazeWM.exe"], creationflags=subprocess.CREATE_NEW_CONSOLE
+)
+
+sleep(1)
+
+
+command_1 = subprocess.run(
+    [r"D:\Source\GlazeWM\GlazeWM.exe", "command", '"focus workspace 9"']
+)
 
 # 打开Edge浏览器的隐私窗口
 subprocess.run(
@@ -16,12 +28,14 @@ subprocess.run(
     ]
 )
 
+command_2 = subprocess.run(
+    [r"D:\Source\GlazeWM\GlazeWM.exe", "command", '"focus workspace 1"']
+)
+
 while True:
-    try:
-        edge = gw.getWindowsWithTitle(
-            "C0BF384C333E479C9C01884375881FBC - [InPrivate] - Microsoft​ Edge"
-        )[0]
-        edge.minimize()
+    edge = gw.getWindowsWithTitle(
+        "C0BF384C333E479C9C01884375881FBC - [InPrivate] - Microsoft​ Edge"
+    )
+    if edge != []:
+        edge[0].minimize()
         break
-    except IndexError:
-        pass
